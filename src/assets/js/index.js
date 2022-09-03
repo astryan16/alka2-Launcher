9afa17fb0830e1cf40018dd4ef991d70f3762921
astryan16 <112553837+astryan16@users.noreply.github.com>
2022-09-03 14:02:42 +0200

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Skyblock modée", "author": "Astryan" },
+            { "message": "faction.", "author": "Astryan" },
+            { "message": "rejoint nous vite.", "author": "Astryan" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
